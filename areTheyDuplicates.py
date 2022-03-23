42ad1872c0e2a350d0c4690c91cceafbacdfdbf4
Michael Mugadza <mikemunko@gmail.com>
2022-03-23 16:10:17 +0200

    used multiple pointers method

--- areTheyDuplicates.py
+++ areTheyDuplicates.py
@@ -1,4 +1,4 @@
-# implement a function called areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.
+# frequency counter method
 def areThereDuplicates(*args):
     dict1 = {}
     for i in args:
@@ -6,4 +6,12 @@ def areThereDuplicates(*args):
         if dict1[i] > 1:
             return True
 
+    return False
+
+# multiple pointers method
+def areThereDuplicates2(*args):
+    args = sorted(args)
+    for i in range(len(args)-1):
+        if args[i] == args[i+1]:
+            return True
     return False
